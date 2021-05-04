e4d2afa823fa4f2c79f59abaadd7a35f74ae2da0
zachkohl <zach.kohl@gmail.com>
2021-05-04 12:17:35 -0700

    updated image

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Image
-        src="/4D525F8A-9952-4854-8906-1A9BD2E41BDE.jpg"
-        alt="voter guide"
-        width={500}
-        height={500}
-      />
+      <Image src="/flyer.jpg" alt="voter guide" width={500} height={500} />
     </div>
   );
 }
