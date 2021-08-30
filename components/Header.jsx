bdaff166faa5043a83f85e87757d92d5c06e0613
Luke Rutan <luke.rutan@gmail.com>
2021-08-29 23:48:23 -0700

    getting familiar + reorganizing

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+
+function Header() {
+  return (
+    <header>
+
+    </header>
+  );
+}
+
+export default Header;
