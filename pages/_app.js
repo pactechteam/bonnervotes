74ae0576edb853e2bc6a993cb1ae60d855e821e0
lukerutan <89704078+lukerutan@users.noreply.github.com>
2021-10-31 05:42:21 -0700

    final improvements
    updated favicon, reordered candidates by position, mobile improvements.

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,7 @@ import '../styles/utils.scss';
 /**
  * Site Layout
  */
+
 const Layout = ({ children }) => {
   return (
     <div id="wrapper">
@@ -26,6 +27,7 @@ const Layout = ({ children }) => {
 /**
  * Application Component
  */
+
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
   // TODO: Move into a hook
@@ -45,6 +47,23 @@ export default function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" key="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ffffff"/>
         {/* Adobe Fonts */}
         <script src="//use.typekit.net/ics4lea.js"></script>
         <script>{"try{Typekit.load({ async: true });}catch(e){ }"}</script>
